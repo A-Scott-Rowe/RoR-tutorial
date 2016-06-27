e9d3315b5524a4cbe79a3a6a949cccf2925f8cfc
Scott Rowe <a.scott.rowe@gmail.com>
2016-06-27 00:59:55 -0400

    finish up statis pages

--- Gemfile
+++ Gemfile
@@ -25,6 +25,8 @@ gem 'sdoc', '~> 0.4.0', group: :doc
 # use RSpec for tests
 gem 'rspec-rails'
 
+gem 'rubocopter'
+
 # Use ActiveModel has_secure_password
 # gem 'bcrypt', '~> 3.1.7'
 
--- spec/controllers/static_pages_controller_spec.rb
+++ spec/controllers/static_pages_controller_spec.rb
@@ -1,19 +1,30 @@
 require 'rails_helper'
 
 RSpec.describe StaticPagesController, type: :controller do
+  
+  render_views
 
-  describe "GET #home" do
-    it "returns http success" do
+  describe 'GET #home' do
+    it 'returns http success' do
       get :home
       expect(response).to have_http_status(:success)
+      response.body.should include('Home | Ruby on Rails Tutorial Sample App')
     end
   end
 
-  describe "GET #help" do
-    it "returns http success" do
+  describe 'GET #help' do
+    it 'returns http success' do
       get :help
       expect(response).to have_http_status(:success)
+      response.body.should include('Help | Ruby on Rails Tutorial Sample App')
     end
   end
 
+  describe 'GET #about' do
+    it 'returns http success' do
+      get :about
+      expect(response). to have_http_status(:success)
+      response.body.should include('About | Ruby on Rails Tutorial Sample App')
+    end
+  end
 end
